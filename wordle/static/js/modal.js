c2c43689fa3b46fa67b12ef23a885edd8691a697
maya <happymomo53@gmail.com>
2025-06-16 15:45:10 +0900

    modal 수정

---
+++ wordle/static/js/modal.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("howto-modal");
+    const btn = document.getElementById("howto-btn");
+    const span = document.querySelector(".close-btn");
+
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    }
+});
